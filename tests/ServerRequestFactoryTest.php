ab3b17e40e57f96c5b837bedb0f24e4c0e008ea5
Adinan Cenci <adinancenci@protonmail.com>
2025-06-04 23:02:22 +0100

    issue-13: expanded ServerRequestFactoryTest

--- tests/ServerRequestFactoryTest.php
+++ tests/ServerRequestFactoryTest.php
@@ -78,4 +78,83 @@ class ServerRequestFactoryTest extends TestCase
         $this->assertInstanceOf(UploadedFileInterface::class, $uploadedFiles['enemies']['final_boss']);
         $this->assertEquals('sauron.jpg', $uploadedFiles['enemies']['final_boss']->getClientFilename());
     }
+
+    public function testParseJson()
+    {
+        $json = '{"series":"The Lord of the Rings","author":"J. R. R. Tolkien"}';
+
+        $uriFactory = new UriFactory();
+        $streamFactory = new StreamFactory();
+
+        $uri  = $uriFactory->createUri('https://mytest.com/file.json');
+        $body = $streamFactory->createStream($json);
+
+        $request = new ServerRequest(
+            '1.1',
+            [
+                'Content-type' => 'application/json',
+            ],
+            $body,
+            '/',
+            'PUT',
+            $uri,
+            [],
+            [],
+            [],
+            null,
+            [],
+            []
+        );
+
+        $factory = $this->getFactory();
+        $request = $factory->parseBody($request);
+
+        $postData = $request->getParsedBody();
+
+        $this->assertInstanceOf(\stdClass::class, $postData);
+        $this->assertEquals('The Lord of the Rings', $postData->series);
+    }
+
+    public function testParseXml()
+    {
+        $xml = '<?xml version="1.0" encoding="utf-8"?>
+        <series>
+            <book>The Fellowship of the Ring</book>
+            <book>The Two Towers</book>
+            <book>The Return of the King</book>
+        </series>';
+
+        $uriFactory = new UriFactory();
+        $streamFactory = new StreamFactory();
+
+        $uri  = $uriFactory->createUri('https://mytest.com/file.json');
+        $body = $streamFactory->createStream($xml);
+
+        $request = new ServerRequest(
+            '1.1',
+            [
+                'Content-type' => 'text/xml',
+            ],
+            $body,
+            '/',
+            'PUT',
+            $uri,
+            [],
+            [],
+            [],
+            null,
+            [],
+            []
+        );
+
+        $factory = $this->getFactory();
+        $request = $factory->parseBody($request);
+
+        $postData = $request->getParsedBody();
+
+        $this->assertInstanceOf(\SimpleXMLElement::class, $postData);
+        $this->assertEquals('The Fellowship of the Ring', $postData->book[0]);
+        $this->assertEquals('The Two Towers', $postData->book[1]);
+        $this->assertEquals('The Return of the King', $postData->book[2]);
+    }
 }
